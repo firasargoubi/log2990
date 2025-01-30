88253430f0808189b0de25786403d69297d6c204
nawbenesri <nawal.benesrighe@polymtl.ca>
2025-01-29 22:00:31 -0500

    integration finished, css needs to be done asap

--- client/src/app/pages/create-page/create-page.component.ts
+++ client/src/app/pages/create-page/create-page.component.ts
@@ -1,21 +1,22 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { MatCardModule } from '@angular/material/card';
 import { BoxFormDialogComponent } from '@app/components/box-form-dialog/box-form-dialog.component';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
-import { Game, GameCardComponent } from '@app/components/game-card/game-card.component';
+import { GameCreation, GameCreationCardComponent } from '@app/components/game-creation-card/game-creation-card.component';
 
 const API_URL = 'http://localhost:3000/api/game';
 
 @Component({
     selector: 'app-create-page',
     standalone: true,
-    imports: [CommonModule, ReactiveFormsModule, GameCardComponent],
+    imports: [CommonModule, ReactiveFormsModule, GameCreationCardComponent, MatCardModule],
     templateUrl: './create-page.component.html',
     styleUrls: ['./create-page.component.scss'],
 })
 export class CreatePageComponent implements OnInit {
-    @Input() games!: Game[];
+    @Input() games!: GameCreation[];
 
     constructor(private dialog: MatDialog) {}
 
@@ -30,15 +31,15 @@ export class CreatePageComponent implements OnInit {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
 
-        this.games = await response.json();
+        const allGames: GameCreation[] = await response.json();
+        this.games = allGames.filter((game) => game.isVisible);
     }
 
-    onBoxClick(game: Game): void {
+    onBoxClick(game: GameCreation): void {
         const dialogRef = this.dialog.open(BoxFormDialogComponent, {
             width: '400px',
-            data: { boxId: game.id, game }, // Fixed object shorthand
+            data: { boxId: game.id, game },
         });
-
         dialogRef.afterClosed().subscribe((result) => {
             if (result) {
                 // Handle the result, for example: update the game list or process data
