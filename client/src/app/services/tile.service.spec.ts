0761d7b77ecb9ab095e1dac19e69633ccf043d18
nawbenesri <nawal.benesrighe@polymtl.ca>
2025-02-02 21:16:54 -0500

    save-service tests

--- client/src/app/services/tile.service.spec.ts
+++ client/src/app/services/tile.service.spec.ts
@@ -1,19 +1,39 @@
 import { TestBed } from '@angular/core/testing';
-
 import { TileService } from './tile.service';
+import { Tile } from '@app/interfaces/tile';
+import { TileTypes } from '@app/interfaces/tileTypes';
 
 describe('TileService', () => {
     let service: TileService;
-    let tileType: number;
+
     beforeEach(() => {
         TestBed.configureTestingModule({ providers: [TileService] });
         service = TestBed.inject(TileService);
-        tileType = 2;
     });
 
-    it('should copy the type of a tile and save its value', () => {
-        service.copyTileTool(tileType);
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 
-        expect(service.currentTool).toEqual(tileType);
+
+
+    it('should toggle door state when modifying a door tile', () => {
+        service.copyTileTool(TileTypes.DoorClosed);
+
+        const tile: Tile = { type: TileTypes.DoorClosed, x: 2, y: 2, id: '2-2' };
+        service.modifyTile(tile);
+
+        expect(tile.type).toEqual(TileTypes.DoorOpen);
+
+        service.modifyTile(tile);
+
+        expect(tile.type).toEqual(TileTypes.DoorClosed);
+    });
+
+    it('should set tile type to the copied tool if it is not a door', () => {
+
+        const tile: Tile = { type: TileTypes.Wall, x: 3, y: 3, id: '3-3' };
+        service.modifyTile(tile);
+
     });
 });
