f455f87fbe9b243158da46b5c16eaa151011b43c
firasargoubi <firas.argoubi@outlook.com>
2025-02-06 12:35:44 -0500

    Quality of Code started

--- client/src/app/pages/edition-page/edition-page.component.ts
+++ client/src/app/pages/edition-page/edition-page.component.ts
@@ -1,11 +1,12 @@
 import { DragDropModule } from '@angular/cdk/drag-drop';
-import { Component } from '@angular/core';
+import { Component, ElementRef, inject, ViewChild } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { FormsModule } from '@angular/forms';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { BoardComponent } from '@app/components/board/board.component';
 import { ObjectsComponent } from '@app/components/objects/objects.component';
 import { TileOptionsComponent } from '@app/components/tile-options/tile-options.component';
+import { ImageService } from '@app/image.service';
 import { Game } from '@app/interfaces/game.model';
 import { SaveMessage } from '@app/interfaces/saveMessage';
 import { ErrorService } from '@app/services/error.service';
@@ -16,21 +17,34 @@ import { SaveService } from '@app/services/save.service';
     selector: 'app-game-page',
     templateUrl: './edition-page.component.html',
     styleUrls: ['./edition-page.component.scss'],
-    imports: [BoardComponent, TileOptionsComponent, ObjectsComponent, RouterLink, DragDropModule, FormsModule],
+    imports: [FormsModule, BoardComponent, TileOptionsComponent, ObjectsComponent, RouterLink, DragDropModule],
 })
 export class EditionPageComponent {
-    // @ViewChild('board', { static: false }) boardElement: ElementRef;
+    @ViewChild('board', { static: false }) boardElement: ElementRef;
+    game: Game = {
+        id: '',
+        name: '',
+        mapSize: 'small',
+        mode: 'normal',
+        previewImage: '',
+        description: '',
+        lastModified: new Date(),
+        isVisible: true,
+        board: [],
+    };
+
     showErrorPopup: boolean = false;
-    saveSuccessful: boolean = false;
+    saveState: boolean = false;
     errorMessage: string = '';
-    game: Game;
+
+    saveService = inject(SaveService);
+    errorService = inject(ErrorService);
+    gameService = inject(GameService);
+    imageService = inject(ImageService);
 
     constructor(
         private route: ActivatedRoute,
         private router: Router,
-        private gameService: GameService,
-        private saveService: SaveService,
-        private errorService: ErrorService,
     ) {
         this.errorService.message$.pipe(takeUntilDestroyed()).subscribe((message: string) => {
             this.errorMessage += message;
@@ -44,8 +58,21 @@ export class EditionPageComponent {
         this.loadGame();
     }
 
+    get mapSize(): number {
+        switch (this.game.mapSize) {
+            case 'small':
+                return 10;
+            case 'medium':
+                return 15;
+            case 'large':
+                return 20;
+            default:
+                return 10;
+        }
+    }
+
     async saveBoard() {
-        // this.game.previewImage = await this.imageService.captureComponent(this.boardElement.nativeElement);
+        this.game.previewImage = await this.imageService.captureComponent(this.boardElement.nativeElement);
         if (!this.game.name) {
             this.errorService.addMessage('Error: Game name is required.\n');
         }
@@ -55,16 +82,17 @@ export class EditionPageComponent {
 
         this.saveService.alertBoardForVerification(true);
         const saveStatus = this.saveService.currentStatus;
+
         let key: keyof SaveMessage;
         for (key in saveStatus) {
             if (!saveStatus[key]) {
                 this.errorService.addMessage(`Error: ${key} name is not respected.\n`);
-                this.saveSuccessful = false;
             }
         }
+
         if (!this.showErrorPopup) {
             this.saveService.saveGame(this.game);
-            this.saveSuccessful = true;
+            this.saveState = true;
             this.errorService.addMessage('Game saved successfully.\n');
         }
     }
@@ -77,7 +105,7 @@ export class EditionPageComponent {
         if (this.game.id) {
             this.gameService.fetchGameById(this.game.id).subscribe({
                 next: (gameSearched) => {
-                    this.game = gameSearched;
+                    this.game = { ...gameSearched };
                 },
             });
         } else {
@@ -89,9 +117,9 @@ export class EditionPageComponent {
     closePopup() {
         this.errorMessage = '';
         this.showErrorPopup = false;
-        if (this.saveSuccessful) {
+        if (this.saveState) {
             this.router.navigate(['/admin']);
         }
-        this.saveSuccessful = false;
+        this.saveState = false;
     }
 }
