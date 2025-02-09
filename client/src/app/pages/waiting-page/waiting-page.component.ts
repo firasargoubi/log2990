5617ecb68f79c307849c16a6b354506eca962a62
Melissakh07 <144280024+Melissakh07@users.noreply.github.com>
2025-02-09 00:29:07 -0500

    game creation and waiting page components with improved styling and logo integration in waiting page

--- client/src/app/pages/waiting-page/waiting-page.component.ts
+++ client/src/app/pages/waiting-page/waiting-page.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 const RANDOM = 1000;
 const RANDOM_MAX = 9000;
 
 @Component({
     selector: 'app-waiting-page',
+    imports: [RouterLink],
     templateUrl: './waiting-page.component.html',
     styleUrls: ['./waiting-page.component.scss'],
 })
