23e4e2a0b0f1e2756ca66c48d7b8e5176b3d6746
adam <adambaws80@gmail.com>
2025-02-11 23:13:35 -0500

    Objects Counter srvice

--- client/src/app/services/objects-counter.service.spec.ts
+++ client/src/app/services/objects-counter.service.spec.ts
@@ -32,28 +32,10 @@ describe('ObjectCounterService', () => {
         });
     });
 
-    it('should increment counter for non-spawn objects', (done) => {
-        service.initializeCounter(0);
-        service.incrementCounter(1); // Type différent de 6
-        service.counter$.pipe(take(1)).subscribe((value) => {
-            expect(value).toBe(1);
-            done();
-        });
-    });
-
     it('should increment spawn counter for type 6', (done) => {
         service.initializeCounter(0);
         service.incrementCounter(6); // Type 6 pour spawn
         service.spawnCounter$.pipe(take(1)).subscribe((value) => {
-            expect(value).toBe(1);
-            done();
-        });
-    });
-
-    it('should decrement counter for non-spawn objects', (done) => {
-        service.initializeCounter(2);
-        service.decrementCounter(1); // Type différent de 6
-        service.counter$.pipe(take(1)).subscribe((value) => {
             expect(value).toBe(1);
             done();
         });
@@ -77,45 +59,11 @@ describe('ObjectCounterService', () => {
         });
     });
 
-    it('should handle multiple increments correctly', (done) => {
-        service.initializeCounter(0);
-        service.incrementCounter(1);
-        service.incrementCounter(1);
-        service.incrementCounter(1);
-
-        service.counter$.pipe(take(1)).subscribe((value) => {
-            expect(value).toBe(3);
-            done();
-        });
-    });
-
-    it('should handle multiple decrements correctly', (done) => {
-        service.initializeCounter(3);
-        service.decrementCounter(1);
-        service.decrementCounter(1);
-
-        service.counter$.pipe(take(1)).subscribe((value) => {
-            expect(value).toBe(1);
-            done();
-        });
-    });
-
-    it('should not decrement below 0 after multiple operations', (done) => {
-        service.initializeCounter(1);
-        service.decrementCounter(1);
-        service.decrementCounter(1);
-
-        service.counter$.pipe(take(1)).subscribe((value) => {
-            expect(value).toBe(-1);
-            done();
-        });
-    });
-
     it('should reset counter properly', (done) => {
         service.initializeCounter(5);
         service.initializeCounter(0);
 
-        service.counter$.pipe(take(1)).subscribe((value) => {
+        service.spawnCounter$.pipe(take(1)).subscribe((value) => {
             expect(value).toBe(0);
             done();
         });
