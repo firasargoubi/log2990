4ec8b2c964cadfe85ac1781e45f31bb4cd29877a
yassine <yassine.essaouab@polymtl.ca>
2025-02-24 21:38:01 -0500

    feat: Improve lobby ID generation to ensure uniqueness

--- client/src/app/services/lobby.service.ts
+++ client/src/app/services/lobby.service.ts
@@ -37,7 +37,12 @@ export class LobbyService {
     }
 
     private generateId(): string {
-        const code = Math.floor(Math.random() * 10000);
-        return code.toString().padStart(4, '0');
+        let id: string;
+        do {
+            id = Math.floor(Math.random() * 10000)
+                .toString()
+                .padStart(4, '0');
+        } while (this.lobbies.has(id));
+        return id;
     }
 }
