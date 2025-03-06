5cbd3dc2c6bbe8e42ff9aa1963d0cb6ad7869ad6
adam <adambaws80@gmail.com>
2025-03-06 15:48:20 -0500

    Merged to integrate sockets

--- client/src/main.ts
+++ client/src/main.ts
@@ -6,12 +6,11 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { Routes, provideRouter, withHashLocation } from '@angular/router';
 import { AdminPageComponent } from '@app/pages/admin-page/admin-page.component';
 import { AppComponent } from '@app/pages/app/app.component';
+import { CreatePageComponent } from '@app/pages/create-page/create-page.component';
 import { EditionPageComponent } from '@app/pages/edition-page/edition-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
 import { WaitingPageComponent } from '@app/pages/waiting-page/waiting-page.component';
 import { environment } from './environments/environment';
-import { CreatePageComponent } from '@app/pages/create-page/create-page.component';
-import { ChatComponent  } from '@app/pages/client-webSocket-test-page/socket-client-component';
 
 if (environment.production) {
     enableProdMode();
@@ -28,7 +27,6 @@ const routes: Routes = [
     { path: 'create', component: CreatePageComponent },
     { path: 'waiting', component: WaitingPageComponent },
     { path: 'waiting/:id', component: WaitingPageComponent },
-    { path: 'socketTest', component: ChatComponent },
     { path: '**', redirectTo: '/home' },
 ];
 
