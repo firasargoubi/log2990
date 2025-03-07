e9533f49dd2d2b4b35d9262344c51e32c3d0b851
Firas Argoubi <firas.argoubi@polymtl.ca>
2025-03-07 01:39:37 -0500

    Added Common Files

---
+++ common/game.interface.ts
@@ -0,0 +1,29 @@
+export enum GameType {
+    Classic = 'classic',
+    Capture = 'capture',
+}
+
+export enum GameSize {
+    Small = 'small',
+    Medium = 'medium',
+    Large = 'large',
+}
+
+export interface GameObjectPlacement {
+    id: number;
+    x: number;
+    y: number;
+}
+
+export interface Game {
+    id: string;
+    name: string;
+    mapSize: GameSize;
+    mode: GameType;
+    previewImage: string;
+    description: string;
+    lastModified: Date;
+    isVisible: boolean;
+    board: number[][];
+    objects: GameObjectPlacement[];
+}
