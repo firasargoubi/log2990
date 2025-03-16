408e7dc3b760fd22f4384c1d42b0c9176d980437
firasargoubi <firas.argoubi@outlook.com>
2025-03-16 17:21:19 -0400

    Modified game state

--- common/game-state.ts
+++ common/game-state.ts
@@ -1,14 +1,20 @@
-import { Player } from '@common/player';
 import { Coordinates } from '@common/coordinates';
+import { Player } from '@common/player';
 
 export interface GameState {
-    id: string;                        
-    board: number[][];                            
-    turnCounter: number;               
-    players: Player[];                 
-    currentPlayer: string;              
+    id: string;
+    board: number[][];
+    turnCounter: number;
+    players: Player[];
+    currentPlayer: string;
     availableMoves: Coordinates[];
-    selectedPath: Coordinates[];
-    playerPositions: Map<string, Coordinates>;
+    shortestMoves: Coordinates[][];
+    playerPositions: Coordinates[];
+    spawnPoints: Coordinates[];
     currentPlayerMovementPoints: number;
+    combat?: {
+        playerId: string;
+        isActive: boolean;
+        endTime: Date;
+    };
 }
