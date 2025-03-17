b8209da6ad0225ffdd66423464fb79ff0ceb8dc6
nawbenesri <nawal.benesrighe@polymtl.ca>
2025-03-17 02:33:11 -0400

    QA countdown & inventory

--- client/src/app/components/countdown-combat/countdown-combat.component.spec.ts
+++ client/src/app/components/countdown-combat/countdown-combat.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CountdownCombatComponent } from './countdown-combat.component';
 
 describe('CountdownCombatComponent', () => {
-  let component: CountdownCombatComponent;
-  let fixture: ComponentFixture<CountdownCombatComponent>;
+    let component: CountdownCombatComponent;
+    let fixture: ComponentFixture<CountdownCombatComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [CountdownCombatComponent]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [CountdownCombatComponent],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(CountdownCombatComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(CountdownCombatComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
