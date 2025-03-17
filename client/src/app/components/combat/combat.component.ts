7ab3a78aeefaac2ef254f24dcab8fcba94c5d4ba
nawbenesri <nawal.benesrighe@polymtl.ca>
2025-03-17 07:59:14 -0400

    combat imported

--- client/src/app/components/combat/combat.component.ts
+++ client/src/app/components/combat/combat.component.ts
@@ -1,5 +1,4 @@
-// combat.component.ts
-import { Component, inject, Input, OnChanges, OnDestroy, OnInit } from '@angular/core';
+import { Component, inject, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { LobbyService } from '@app/services/lobby.service';
 import { GameState } from '@common/game-state';
 import { Player } from '@common/player';
@@ -10,7 +9,7 @@ const TO_SECONDS = 1000;
     templateUrl: './combat.component.html',
     styleUrls: ['./combat.component.scss'],
 })
-export class CombatComponent implements OnInit, OnChanges, OnDestroy {
+export class CombatComponent implements OnInit, OnChanges {
     @Input() currentPlayer!: Player;
     @Input() opponent!: Player;
     @Input() lobbyId!: string;
@@ -19,14 +18,8 @@ export class CombatComponent implements OnInit, OnChanges, OnDestroy {
     playerTurn: string = '';
     countDown: number = 0;
     canAct: boolean = false;
-    isCombatCountdownActive: boolean = false;
-    combatTimeLeft: number = 5; // Temps de combat par tour
-    playerTimeLeft: number = 60; // Temps du joueur
-    pausedPlayerTimeLeft: number = 0; // Temps restant du joueur avant la pause
-
     private lobbyService = inject(LobbyService);
-    private combatTimer: ReturnType<typeof setInterval> | null = null;
-    private playerTimer: ReturnType<typeof setInterval> | null = null;
+    private countDownInterval: ReturnType<typeof setInterval> | null = null;
 
     ngOnInit() {
         if (this.gameState) {
@@ -46,62 +39,83 @@ export class CombatComponent implements OnInit, OnChanges, OnDestroy {
         if (!this.opponent) {
             this.opponent = this.gameState?.players.find((p) => p.id !== this.currentPlayer.id) ?? this.opponent;
         }
+        this.startCountdown();
     }
 
-    startCombatCountdown(): void {
-        this.isCombatCountdownActive = true;
-
-        // Sauvegarder le temps restant du joueur et mettre en pause son compteur
-        this.pausedPlayerTimeLeft = this.playerTimeLeft;
-        this.stopPlayerCountdown();
-
-        // Démarrer le compteur de combat
-        this.combatTimer = setInterval(() => {
-            this.combatTimeLeft--;
-            if (this.combatTimeLeft <= 0) {
-                this.stopCombatCountdown();
+    startCountdown() {
+        this.countDownInterval = setInterval(() => {
+            if (this.countDown > 0) {
+                this.countDown--;
+            } else {
+                this.endTimer();
             }
-        }, 1000);
+        }, TO_SECONDS);
     }
 
-    stopCombatCountdown(): void {
-        // Arrêter le compteur de combat
-        if (this.combatTimer) {
-            clearInterval(this.combatTimer);
+    endTimer() {
+        if (this.gameState) {
+            this.lobbyService.changeTurnEnd(this.currentPlayer, this.opponent, this.playerTurn, this.gameState);
         }
-        this.isCombatCountdownActive = false;
-
-        // Reprendre le compteur du joueur à partir du temps sauvegardé
-        this.playerTimeLeft = this.pausedPlayerTimeLeft;
-        this.startPlayerCountdown();
+        if (this.countDownInterval !== null) {
+            clearInterval(this.countDownInterval);
+        }
+        this.subscribeToPlayerSwitch();
     }
 
-    startPlayerCountdown(): void {
-        this.playerTimer = setInterval(() => {
-            if (this.canAct) {
-                this.playerTimeLeft--;
-                if (this.playerTimeLeft <= 0) {
-                    this.stopPlayerCountdown();
-                }
+    onAttack() {
+        console.log('ON EST DANS ONATTACK() CLIENT');
+        const attackRoll = this.rollDice(this.currentPlayer, 'attack') + this.currentPlayer.attack;
+        const defenseRoll = this.rollDice(this.opponent, 'defense') + this.opponent.defense;
+        if (this.isOnIce(this.currentPlayer)) {
+            console.log("L'attaquant se trouve sur une tuile de glace");
+            this.currentPlayer.attack -= 2;
+            this.currentPlayer.defense -= 2;
+        }
+        if (this.isOnIce(this.opponent)) {
+            this.opponent.attack -= 2;
+            this.opponent.defense -= 2;
+        }
+        const damage = attackRoll - defenseRoll;
+        console.log('dommage appliqué ', damage);
+        if (damage > 0) {
+            this.opponent.life -= damage;
+            console.log("La vie de l'opposant: ", this.opponent.life);
+            if (this.opponent.life <= 0) {
+                this.handleDefeat(this.opponent);
             }
-        }, 1000);
+        } else {
+            console.log("L'attaque n'a pas fonctionnée");
+        }
+
+        this.lobbyService.attackAction(this.lobbyId, this.opponent, damage, this.opponent.life);
+        this.subscribeChangeAttributes();
     }
 
-    stopPlayerCountdown(): void {
-        if (this.playerTimer) {
-            clearInterval(this.playerTimer);
-            this.playerTimer = null; // Nettoyer la référence
-        }
+    private rollDice(player: Player, type: 'attack' | 'defense'): number {
+        const diceType = player.bonus?.[type] === 'D4' ? 4 : 6;
+        return Math.floor(Math.random() * diceType) + 1;
     }
 
-    ngOnDestroy(): void {
-        // Nettoyer les intervalles lors de la destruction du composant
-        if (this.combatTimer) {
-            clearInterval(this.combatTimer);
+    private isOnIce(player: Player): boolean {
+        let tileType;
+        const currentPlayerIndex = this.gameState?.players.findIndex((p) => p.id === player.id);
+        if (currentPlayerIndex !== undefined && currentPlayerIndex !== -1) {
+            const currentPlayerPositon = this.gameState?.playerPositions[currentPlayerIndex];
+            if (this.gameState && currentPlayerPositon) {
+                tileType = this.gameState.board[currentPlayerPositon.x][currentPlayerPositon.y] % 10;
+            }
         }
-        if (this.playerTimer) {
-            clearInterval(this.playerTimer);
+        if (tileType === 3) {
+            return true;
         }
+        return false;
+    }
+
+    private handleDefeat(player: Player) {
+        if (this.gameState) {
+            this.lobbyService.handleDefeat(player, this.lobbyId);
+        }
+        this.subscribeToNewSpawnPoints();
     }
 
     private subscribeToPlayerTurn() {
@@ -109,6 +123,44 @@ export class CombatComponent implements OnInit, OnChanges, OnDestroy {
             this.playerTurn = data.playerTurn;
             this.countDown = data.countDown;
             this.canAct = this.currentPlayer.id === this.playerTurn;
+        });
+    }
+
+    private subscribeToPlayerSwitch() {
+        this.lobbyService.getPlayerSwitch().subscribe((data) => {
+            this.playerTurn = data.newPlayerTurn;
+            this.countDown = data.countDown;
+            this.canAct = this.currentPlayer.id === this.playerTurn;
+        });
+    }
+
+    private subscribeToNewSpawnPoints() {
+        this.lobbyService.newSpawnPoints().subscribe((data) => {
+            const playerIndex = this.gameState?.players.findIndex((p) => p.id === data.player.id) ?? -1;
+            if (playerIndex === -1) return;
+            if (this.gameState && this.gameState.playerPositions) {
+                this.gameState.playerPositions[playerIndex] = data.newSpawn;
+            }
+        });
+    }
+
+    private subscribeChangeAttributes() {
+        console.log('coucou on a changé les attributs dans subscribe');
+        this.lobbyService.updateHealth().subscribe((data) => {
+            console.log('Ça rentre ici ? ');
+            const opponent = this.gameState?.players.find((p) => p.id === data.player.id);
+            if (opponent) {
+                opponent.life = data.remainingHealth;
+                console.log('data.remainingHealth ', data.remainingHealth);
+                console.log('opponent.life: ', opponent.life);
+            }
+        });
+
+        this.lobbyService.updatePlayerTurn().subscribe((data) => {
+            this.playerTurn = data.nextPlayer.id;
+            this.canAct = this.currentPlayer.id === this.playerTurn;
+            console.log('new player turn: ', this.playerTurn);
+            console.log('canAct: ', this.canAct);
         });
     }
 }
