38f8d3d365ce5e88c2d847b5f6feeccab10c3658
nawbenesri <nawal.benesrighe@polymtl.ca>
2025-03-17 07:33:53 -0400

    merge

--- client/src/app/components/inventory/inventory.component.ts
+++ client/src/app/components/inventory/inventory.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
 @Component({
     selector: 'app-inventory',
     templateUrl: './inventory.component.html',
@@ -7,5 +8,17 @@ import { Component } from '@angular/core';
     imports: [CommonModule],
 })
 export class InventoryComponent {
-    inventory = ['Espace 1: Vide', 'Espace 2: Vide']; // Espaces de stockage vides pour le moment
+    inventory = ['Espace 1: Vide', 'Espace 2: Vide'];
+
+    pickUpItem(item: string): void {
+        const emptySlotIndex = this.inventory.findIndex((slot) => slot.includes('Vide'));
+
+        if (emptySlotIndex !== -1) {
+            this.inventory[emptySlotIndex] = `${item} (Ramassé)`;
+        }
+    }
+
+    resetInventory(): void {
+        this.inventory = ['Espace 1: Vide', 'Espace 2: Vide'];
+    }
 }
