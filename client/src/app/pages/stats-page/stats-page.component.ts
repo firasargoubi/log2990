4b3dae43330f8304e054e3da39c06897875e23e0
adam <adambaws80@gmail.com>
2025-04-09 14:08:16 -0400

    Add StatsPage component with HTML, SCSS, and unit tests

---
+++ client/src/app/pages/stats-page/stats-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-stats-page',
+    imports: [],
+    templateUrl: './stats-page.component.html',
+    styleUrls: ['./stats-page.component.scss'],
+    standalone: true,
+})
+export class StatsPageComponent {}
